e5b2a4266f62f158f8360fab4e6cb23c3578af9a
neghani <neghani@gmail.com>
2016-02-15 01:57:40 +0530

    init server

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3001;
+
+app.get('/',function(req,res){
+
+	res.send("Root folder");
+})
+
+app.listen(port,function (){
+
+	console.log("server is running..")
+})
